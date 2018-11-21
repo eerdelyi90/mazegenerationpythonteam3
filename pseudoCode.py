cb1e4241ee01e58f0936359f4c33b847ee98dced
Egon Erdelyi <egon.erdelyi@redforestia.com>
2018-11-21 21:28:01 +0100

    maze node pseudo

--- pseudoCode.py
+++ pseudoCode.py
@@ -8,6 +8,7 @@ from time import sleep
 sizex = 8
 sizey = 8
 numberOfSteps = 25
+currenstep = 0
 directions = [[0,1],[1,0],[-1,0],[0,-1]]
 
 
@@ -15,28 +16,51 @@ MazeMatrix = [[0 for x in range(sizex)] for y in range(sizey)]
 mazeNodeList = []
 
 class MazeNode:
-	value = False
+	nextNode = None
 	def __init__(self,x,y,previousMazeNode,isCurrent):
 		self.x = x
 		self.y = y
 
-	def setMazeValue():
+	def setMazeValue(value):
 		MazeMatrix[self.x][self.y] = value
 
 	def hasNoOptions():
 		#has no where to go
 
+	def createNextMazeNode():
+		#pick a direction
+		#create next node
+		#return nextnode 
+
+
+
+
 
 
 # loop until done (PROBS A WHILE loop)
-for x in range(numberOfSteps):
-	if (mazeNodeList.length <= 0):
-		currentNode = MazeNode(0,0,None,True)
-		mazeNodeList.append(currentNode)
-	
-	#currentNode.setMazeValue()
-	if(currentNode.hasNoOptions()):
-		currentNode = currentNode.previousMazeNode
+def runMaze():
+	if (currenstep < numberOfSteps ):
+		currenstep++
+		if (mazeNodeList.length <= 0):
+			currentNode = MazeNode(0,0,None,True)
+			mazeNodeList.append(currentNode)
+		
+		#currentNode.setMazeValue()
+		if(currentNode.hasNoOptions()):
+			currentNode = currentNode.previousMazeNode
+		else:
+			currentNode.setMazeValue(True)
+			nextNode = currentNode.createNextMazeNode()
+			currentNode = nextnode
+			mazeNodeList.append(currentNode)
+			runMaze()
+	else:
+		#stop
+
+
+
+
+
 
 
 	
